94dcb9d6b6df397f28c02626d151ed1ee9a5ef5c
npv00 <vennaro@gmail.com>
2020-04-27 10:01:10 -0400

    Initial Commit

---
+++ TestGIT01/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TestGIT01
+//
+//  Created by Nicholas Vennaro on 4/27/20.
+//  Copyright © 2020 npv. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
